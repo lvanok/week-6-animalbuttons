5d4fb7099410a3256116e3cbb9e52e6fdbba561a
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-03-14 15:38:54 -0400

    week6hw.html

---
+++ hw.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<style>
+        $('button').on('click', function() {
+        var animalIn = $(this).data('animalIn');
+        animalIn.attr('data-animal', animals[i]);
+        animalIn.text()animals[i]);
+        $("#animal-form").append(animalIn);
+        });
+
+</style>
+</html>
